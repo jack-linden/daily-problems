1725f489e323df12bce0f5651a670842d699f7ab
Jack Linden <Linden_John@bah.com>
2020-02-27 20:10:57 -0500

    committing StairCase countWays dynamic programming solutions. one w/ an array and one w/ constant space

--- staircase-hard/src/main/java/staircase/StairCase.java
+++ staircase-hard/src/main/java/staircase/StairCase.java
@@ -57,5 +57,33 @@ public class StairCase {
         curr.remove(curr.size()-1);
     }
 
+
+    public int countWaysDynamic(int n){
+        
+
+        int[] arr = new int[n+1];
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for( int i = 2; i <=n ; i++ ){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        return arr[n];
+    }
+
+    public int countWaysDynamicConstantSpace(int n){
+        int f = 1;
+        int g = 1;
+        int cnt = 1;
+        for( int i = 2; i <=n ; i++ ){
+            cnt = f + g;
+            f = g;
+            g = cnt;
+        }
+
+        return cnt;
+    }
+
    
 }
